d3669f74f9947e55cec10336d60fdeda9adac2c4
Rizky Pratama <90410548+rtamaaa@users.noreply.github.com>
2023-10-28 15:13:32 +0700

    Add files via upload

---
+++ db_penjualan (1).sql
@@ -0,0 +1,262 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 28, 2023 at 10:12 AM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 7.4.27
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_penjualan`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `barang`
+--
+
+CREATE TABLE `barang` (
+  `kode_barang` varchar(11) NOT NULL,
+  `nama_barang` varchar(255) NOT NULL,
+  `harga` decimal(10,2) NOT NULL,
+  `qty` int(11) DEFAULT NULL,
+  `id_kategori` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `barang`
+--
+
+INSERT INTO `barang` (`kode_barang`, `nama_barang`, `harga`, `qty`, `id_kategori`) VALUES
+('br0001', 'pensil', '2000.00', 20, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kategori`
+--
+
+CREATE TABLE `kategori` (
+  `id_kategori` int(11) NOT NULL,
+  `nama_kategori` char(50) NOT NULL,
+  `diskon` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `kategori`
+--
+
+INSERT INTO `kategori` (`id_kategori`, `nama_kategori`, `diskon`) VALUES
+(1, 'ATK', 5),
+(2, 'Elektronik', 10),
+(3, 'Sembako', 20);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `level`
+--
+
+CREATE TABLE `level` (
+  `id_level` int(11) NOT NULL,
+  `level` varchar(20) NOT NULL,
+  `diskon_level` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `level`
+--
+
+INSERT INTO `level` (`id_level`, `level`, `diskon_level`) VALUES
+(1, 'Gold', 10),
+(2, 'Silver', 5),
+(3, 'Platinum', 15);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `member`
+--
+
+CREATE TABLE `member` (
+  `kode_pelanggan` varchar(11) NOT NULL,
+  `nama_pelanggan` varchar(30) NOT NULL,
+  `id_level` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `member`
+--
+
+INSERT INTO `member` (`kode_pelanggan`, `nama_pelanggan`, `id_level`) VALUES
+('PL0001', 'mahmud', 1),
+('PL0002', 'roni', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `penjualan`
+--
+
+CREATE TABLE `penjualan` (
+  `id_penjualan` int(11) NOT NULL,
+  `tgl_penjualan` date NOT NULL,
+  `nama_pelanggan` varchar(50) NOT NULL,
+  `kode_barang` varchar(11) NOT NULL,
+  `qty` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `penjualan`
+--
+
+INSERT INTO `penjualan` (`id_penjualan`, `tgl_penjualan`, `nama_pelanggan`, `kode_barang`, `qty`) VALUES
+(2, '2023-10-28', 'roni', 'br0001', 12);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaksi`
+--
+
+CREATE TABLE `transaksi` (
+  `tgl_transaksi` date NOT NULL,
+  `no_transaksi` varchar(20) NOT NULL,
+  `jenis_transaksi` char(50) NOT NULL,
+  `kode_barang` varchar(11) NOT NULL,
+  `jml_barang` int(11) NOT NULL,
+  `diskon` int(11) NOT NULL,
+  `total_transaksi` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `transaksi`
+--
+
+INSERT INTO `transaksi` (`tgl_transaksi`, `no_transaksi`, `jenis_transaksi`, `kode_barang`, `jml_barang`, `diskon`, `total_transaksi`) VALUES
+('2023-10-27', '6t87587', 'Penjualan', 'br0007', 2, 0, 0),
+('2023-10-27', '67467476', 'Member', 'br0007', 1, 0, 0),
+('2023-10-27', '908979', 'Member', 'br0007', 12, 0, 0),
+('2023-10-27', '908979', 'Member', 'br0007', 12, 0, 0),
+('2023-10-27', '6t87686', 'Member', 'br0007', 2, 0, 0),
+('2023-10-28', '908979', 'Member', 'br0007', 12, 0, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id_user` int(11) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `password` varchar(16) NOT NULL,
+  `level` int(11) NOT NULL,
+  `status` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id_user`, `nama`, `password`, `level`, `status`) VALUES
+(2, 'parman', '123', 2, 0),
+(5, 'surya', '123', 1, 1),
+(6, 'wahyudi', '123', 1, 1),
+(7, 'roni', '123', 1, 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `barang`
+--
+ALTER TABLE `barang`
+  ADD PRIMARY KEY (`kode_barang`),
+  ADD KEY `id_kategori` (`id_kategori`);
+
+--
+-- Indexes for table `kategori`
+--
+ALTER TABLE `kategori`
+  ADD PRIMARY KEY (`id_kategori`);
+
+--
+-- Indexes for table `level`
+--
+ALTER TABLE `level`
+  ADD PRIMARY KEY (`id_level`);
+
+--
+-- Indexes for table `member`
+--
+ALTER TABLE `member`
+  ADD PRIMARY KEY (`kode_pelanggan`);
+
+--
+-- Indexes for table `penjualan`
+--
+ALTER TABLE `penjualan`
+  ADD PRIMARY KEY (`id_penjualan`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `kategori`
+--
+ALTER TABLE `kategori`
+  MODIFY `id_kategori` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `level`
+--
+ALTER TABLE `level`
+  MODIFY `id_level` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `penjualan`
+--
+ALTER TABLE `penjualan`
+  MODIFY `id_penjualan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `barang`
+--
+ALTER TABLE `barang`
+  ADD CONSTRAINT `barang_ibfk_1` FOREIGN KEY (`id_kategori`) REFERENCES `kategori` (`id_kategori`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
